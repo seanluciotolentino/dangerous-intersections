031c4fca624c589f9c91b0ef486f4196bbc0fda4
Lucio Tolentino <seanluciotolentino@gmail.com>
2015-03-19 15:45:07 -0400

    Added functionality for selecting districts and times

--- host_data.py
+++ host_data.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 import math
 import pandas as pd
 import os
+import datetime
+import numpy as np
+import time
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -18,38 +21,59 @@ for i in range(len(cols)): key[cols[i]] = i
 """
 key = {'NUMBER OF CYCLIST KILLED': 0, 'ZIP CODE': 5, 'FOUR_SQUARE_DRINK': 21, 'YELP_SHOW': 20, 'YELP_DRINK': 17, 'OFF STREET NAME': 15, 'NUMBER OF MOTORIST KILLED': 8, 'YELP_MUSIC': 19, 'ON STREET NAME': 1, 'CROSS STREET NAME': 9, 'YELP_FOOD': 18, 'NUMBER OF MOTORIST INJURED': 4, 'LONGITUDE': 6, 'NUMBER OF PEDESTRIANS INJURED': 11, 'NUMBER OF PEDESTRIANS KILLED': 10, 'CRASHES': 2, 'NUMBER OF PERSONS KILLED': 3, 'NUMBER OF CYCLIST INJURED': 7, 'YELP_BAR': 16, 'LATITUDE': 12, 'NUMBER OF PERSONS INJURED': 13, 'BOROUGH': 14, 'FOUR_SQUARE_FOOD': 22}
 
+
 @app.route("/")
 def default():
     return jsonify({"msg":"Flask API working!"})
     
 @app.route("/explore/")
-@app.route("/explore/<danger>&<n>", methods=['GET'])
-def explore(danger=None, n=None):
+@app.route("/explore/<danger>&<n>&<loc_type>&<loc_spec>&<start>&<end>", methods=['GET'])
+def explore(danger=None, n=None, loc_type=None, loc_spec=None, start=0, end=0):
     #sort the intersection data by danger
-    df = pd.read_csv('crash_data/intersections.csv')
-    df.sort(danger, ascending=False, inplace=True)
-    df.to_csv('crash_data/intersections.csv', index=False)
+    #return jsonify({'loc_type':loc_type, 'loc_spec':loc_spec,
+    #                'start':start, 'end':end})
+    start_time = time.time()
+    #get for a specific time slice
+    if start == '0':  # no time slice -- use pre-grouped
+        df = pd.read_csv('crash_data/intersections.csv')
+    else:
+        # get specific time frame
+        global crashes
+        s = datetime.datetime.strptime(start, '%b %Y')
+        e = datetime.datetime.strptime(end, '%b %Y')+datetime.timedelta(days=30)
+        crashes = crashes[(crashes.DATE >= s) & (crashes.DATE <= e)]
+        if len(crashes) == 0:  # no crashes in that time frame
+            print "Zero crashes for that time period"
+            return jsonify({'markers':[], 'success':False})
+
+        # do the grouping procedure
+        df = group(crashes)
+        
+    #get for the specific location
+    if loc_type == "CITYWIDE":
+        intersections = df.sort(danger, ascending=False).head(n=int(n)).iterrows()
+    else:
+        if loc_type is not "BOROUGH":
+            loc_spec = int(loc_spec)
+        intersections = df[df[loc_type]==loc_spec].sort(danger, ascending=False).head(n=int(n)).iterrows()
     
     #get top n and make them into markers
-    j = {'markers':[]}
-    f = open('crash_data/intersections.csv', 'r')
-    f.readline()  # skip header
-    count = 0
-    for line in f:
-        count+=1
-        if count > int(n):
-            break
-        line = line.strip().split(',')
-        lat = float(line[key['LATITUDE']])
-        lon = float(line[key['LONGITUDE']])
-        main_text = "<b>{0}</b><br>{1}".format(count, line[key[danger]])
+    j = {'markers':[], 'success':True}
+    rank=0
+    for intersection in intersections:
+        lat = intersection[1].LATITUDE
+        lon = intersection[1].LONGITUDE
+        rank+=1
+        main_text = "<b>{0}</b><br>{1}".format(rank, intersection[1][danger])
         popup_text = """<b>Rank: {0}</b><br>
                         {1}: {2}<br>
-                        Street: {3}"""
-        popup_text = popup_text.format(count,
+                        Street 1: {3}<br>
+                        Street 2: {4}"""
+        popup_text = popup_text.format(rank,
                                        danger,
-                                       line[key[danger]], 
-                                       line[key['ON STREET NAME']])
+                                       intersection[1][danger], 
+                                       intersection[1]["ON STREET NAME"],
+                                       intersection[1]["CROSS STREET NAME"])
     
         j['markers'].append({'lon':lon, 
                              'lat':lat, 
@@ -58,6 +82,41 @@ def explore(danger=None, n=None):
                              'color':'#%02x%02x%02x' % (255, 0, 0)
                              })
     return jsonify(j)
+
+def group(crashes):
+    """
+    Helper function for doing the groupby operation when looking
+    for crashes within a specific time slice.
+    """
+    #add index and crash counter
+    crashes['INTERSECTION'] = crashes['LATITUDE'].apply(str) + "_" + crashes['LONGITUDE'].apply(str)
+    crashes['CRASHES'] = 1
+    
+    #group and aggregate
+    gb = crashes.groupby(by='INTERSECTION', as_index=False)
+    longest = lambda x: x.get(x.map(lambda x: len(str(x))).idxmax())  # longest string
+    mirror = lambda x: x.iget(0)
+    agg_fun = {u'BOROUGH':mirror,
+               u'DISTRICT':mirror,
+               u'PRECINCT':mirror,
+               u'ZIP CODE':mirror,
+               u'LATITUDE':mirror,
+               u'LONGITUDE':mirror,
+               u'ON STREET NAME':mirror,
+               u'CROSS STREET NAME':mirror,
+               u'OFF STREET NAME':mirror,
+               u'NUMBER OF PERSONS INJURED':np.sum,
+               u'NUMBER OF PERSONS KILLED':np.sum,
+               u'NUMBER OF PEDESTRIANS INJURED':np.sum,
+               u'NUMBER OF PEDESTRIANS KILLED':np.sum,
+               u'NUMBER OF CYCLIST INJURED':np.sum,
+               u'NUMBER OF CYCLIST KILLED':np.sum,
+               u'NUMBER OF MOTORIST INJURED':np.sum,
+               u'NUMBER OF MOTORIST KILLED':np.sum,
+               'CRASHES':np.sum
+               }
+
+    return gb.aggregate(agg_fun)
 
 @app.route("/intersections/")
 @app.route("/intersections/<danger>&<traffic>", methods=['GET'])
@@ -154,5 +213,10 @@ def schools():
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-    #app.run(debug = True)
-    app.run(host='0.0.0.0', port=int(port))
+    #preload crash data
+    print "loading crash data..."
+    crashes = pd.read_csv('crash_data/crashes_clean.csv')
+    crashes.DATE = crashes.DATE.apply(pd.to_datetime)
+    print 'done.'
+    app.run(debug = True)
+    #app.run(host='0.0.0.0', port=int(port))
